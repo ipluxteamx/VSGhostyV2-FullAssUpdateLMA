d0f4c83aed8fd0bb957a59f91ff4f0b1ba278196
iplux <49649756+ipluxteamx@users.noreply.github.com>
2022-08-07 22:47:04 +0200

    omg

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -515,7 +515,7 @@ class MainMenuState extends MusicBeatState
 	{
 		super.beatHit();
 
-		FlxG.camera.zoom = FlxMath.lerp(1, FlxG.camera.zoom, CoolUtil.boundTo(1 - 3.125), 0, 1));
+		FlxG.camera.zoom = FlxMath.lerp(1, FlxG.camera.zoom, CoolUtil.boundTo(1 - 3.125), 0, 1bru);
 	}
 
 	function changeText()
