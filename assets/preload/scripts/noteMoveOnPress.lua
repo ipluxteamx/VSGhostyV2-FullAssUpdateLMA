3e08ec7c53b2f665cb5a178a4b74608ea2ff1202
iplux <49649756+ipluxteamx@users.noreply.github.com>
2022-08-08 16:43:41 +0200

    Update noteMoveOnPress.lua

--- assets/preload/scripts/noteMoveOnPress.lua
+++ assets/preload/scripts/noteMoveOnPress.lua
@@ -1,102 +1,89 @@
-local strumLinePlayer
-local strumLineOpponent
-local sicks
-local goods
-local bads
-local shits
-local moveAmount = 20
-local tweenAmount = 0.1
+--wow look another cam follow script
+--epnis!! bals!
+
+--Configurations
+local bfCam={50, 200}
+local dadCam={300, 50}
+local drawDebugShit=false
+local intensity=10
+local speed=0.5
+
+--dont touch these vars
+local camPosX=0
+local camPosY=0
+
+--Camera Functions
+local func={
+		[0]=function() --[[ LEFT ]]
+			doTweenX('camMoveX', 'camGame.target', camPosX-intensity, speed, 'quadOut')
+			doTweenY('camMoveY', 'camGame.target', camPosY, speed, 'quadOut')
+		end,
+		[1]=function() --[[ DOWN ]]
+			doTweenX('camMoveX', 'camGame.target', camPosX, speed, 'quadOut')
+			doTweenY('camMoveY', 'camGame.target', camPosY+intensity, speed, 'quadOut')
+		end,
+		[2]=function() --[[ UP ]]
+			doTweenX('camMoveX', 'camGame.target', camPosX, speed, 'quadOut')
+			doTweenY('camMoveY', 'camGame.target', camPosY-intensity, speed, 'quadOut')
+		end,
+		[3]=function() --[[ RIGHT ]]
+			doTweenX('camMoveX', 'camGame.target', camPosX+intensity, speed, 'quadOut')
+			doTweenY('camMoveY', 'camGame.target', camPosY, speed, 'quadOut')
+		end,
+		['default']=function() --[[ IDLE ]]
+			doTweenX('camMoveX', 'camGame.target', camPosX, speed*2, 'quadInOut')
+			doTweenY('camMoveY', 'camGame.target', camPosY, speed*2, 'quadInOut')
+		end,
+	}
+
 function onCreatePost()
-    for i = 0, 3 do
-        strumLinePlayer = getPropertyFromGroup('strumLineNotes', i + 4, 'y')
-        strumLineOpponent = getPropertyFromGroup('strumLineNotes', i, 'y')
-    end
-    sicks = getProperty("sicks")
-    goods = getProperty("goods")
-    bads = getProperty("bads")
-    shits = getProperty("shits")
+	makeLuaSprite('playerPos', 'empty',getProperty('boyfriend.x')+bfCam[1], getProperty('boyfriend.y')+bfCam[2])
+		makeGraphic('playerPos', 32, 32, 'FFFFFF')
+	addLuaSprite('playerPos', true)
+		
+	makeLuaSprite('enemyPos', 'empty',getProperty('dad.x')+dadCam[1], getProperty('dad.y')+dadCam[2])
+		makeGraphic('enemyPos', 32, 32, 'FFFFFF')
+	addLuaSprite('enemyPos', true)
+	
+	setProperty('playerPos.alpha', 0)
+	setProperty('enemyPos.alpha', 0)
+	
+	if drawDebugShit then
+		setProperty('playerPos.alpha', 1)
+		setProperty('enemyPos.alpha', 1)
+	end
 end
 
-function onUpdatePost(elapsed)
-    if downscroll then
-        if sicks then
-            moveAmount = 15
-        elseif goods then
-            moveAmount = 10
-        elseif bads then
-            moveAmount = 5
-        elseif shits then
-            moveAmount = 2.5
-        end
-    elseif not downscroll then
-        if sicks then
-            moveAmount = 0 - 15
-        elseif goods then
-            moveAmount = 0 - 10
-        elseif bads then
-            moveAmount = 0 - 5
-        elseif shits then
-            moveAmount = 0 - 2.5
-        end
-    end
-    if getPropertyFromClass('PlayState', 'isPixelStage') then
-        moveAmount = moveAmount * 1.2
-        tweenAmount = 0.01
-    else
-        moveAmount = moveAmount
-        tweenAmount = 0.1
-    end
+function goodNoteHit(id,dir,note,sus)
+	if mustHitSection and func[dir] then
+		camPosX=getProperty('playerPos.x')
+		camPosY=getProperty('playerPos.y')
+		setProperty('cameraSpeed', 0)
+		func[dir]()
+	end
 end
 
-function goodNoteHit(id, noteData, noteType, isSustainNote)
-    for i = 0, 3 do
-        if noteData == i and not isSustainNote then
-            noteTweenY('movePlayer' .. i, i + 4, strumLinePlayer + moveAmount, tweenAmount, 'circOut')
-            runTimer('returnPlayer' .. i, 0.1, 1)
-        end
-    end
+function opponentNoteHit(id,dir,note,sus)
+	if not mustHitSection and func[dir] then
+		camPosX=getProperty('enemyPos.x')
+		camPosY=getProperty('enemyPos.y')
+		setProperty('cameraSpeed', 0)
+		func[dir]()
+	end
 end
 
-function opponentNoteHit(id, noteData, noteType, isSustainNote)
-    for i = 0, 3 do
-        if noteData == i and not isSustainNote then
-            noteTweenY('moveOpponent' .. i, i, strumLineOpponent + moveAmount, tweenAmount, 'circOut')
-            runTimer('returnOpponent' .. i, 0.1, 1)
-        end
-    end
-end
-
-function onCountdownTick(counter)
-	-- counter = 0 -> "Three"
-	-- counter = 1 -> "Two"
-	-- counter = 2 -> "One"
-	-- counter = 3 -> "Go!"
-	-- counter = 4 -> Nothing happens lol, tho it is triggered at the same time as onSongStart i think
-	if prefs.wierdStupidKindaDumb then
-    	for i = 0, 3 do
-        	if counter == 3 then
-            	noteTweenY('movePlayer' .. i, i + 4, strumLinePlayer + moveAmount / 2, tweenAmount, tweenType)
-            	noteTweenY('moveOpponent' .. i, i, strumLineOpponent + moveAmount / 2, tweenAmount, tweenType)
-        	elseif counter == 4 then
-            	runTimer('returnPlayer' .. i, tweenAmount, 1)
-            	runTimer('returnOpponent' .. i, tweenAmount, 1)
-        	end
-    	end
-    end
-end
-
-function onTimerCompleted(tag, loop, loopsLeft)
-    for i = 0, 3 do
-        if tag == 'returnPlayer' .. i then
-            noteTweenY('movePlayer' .. i, i + 4, strumLinePlayer, tweenAmount, 'circOut')
-        elseif tag == 'returnOpponent' .. i then
-            noteTweenY('moveOpponent' .. i, i, strumLineOpponent, tweenAmount, 'circOut')
-        end
-    end
-end
-
-function onSongStart()
-	if getPropertyFromClass('PlayState', 'chartingMode') then
-		debugPrint(Credits.Wip, Credits.ISH52806)
+function onBeatHit()
+	if curBeat%4==0 then
+		if mustHitSection then
+			camPosX=getProperty('playerPos.x')
+			camPosY=getProperty('playerPos.y')
+			setProperty('cameraSpeed', 0)
+			func['default']()
+		else
+			camPosX=getProperty('enemyPos.x')
+			camPosY=getProperty('enemyPos.y')
+			setProperty('cameraSpeed', 0)
+			func['default']()
+		end
 	end
 end
