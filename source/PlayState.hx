515e26a2b9d653714bc7e636f015c8aab4e1556c
iplux <49649756+ipluxteamx@users.noreply.github.com>
2022-08-09 16:43:47 +0200

    25

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3057,7 +3057,8 @@ class PlayState extends MusicBeatState
 		{
 			FlxG.camera.zoom = FlxMath.lerp(defaultCamZoom, FlxG.camera.zoom, CoolUtil.boundTo(1 - (elapsed * 3.125 * camZoomingDecay), 0, 1));
 			camHUD.zoom = FlxMath.lerp(1, camHUD.zoom, CoolUtil.boundTo(1 - (elapsed * 3.125 * camZoomingDecay), 0, 1));
-			FlxG.camera.shake(0.005, 0.5);
+			FlxG.camera.shake(0.003, 0.15);
+			camHUD.shake(0.003, 0.15);
 		}
 
 		FlxG.watch.addQuick("secShit", curSection);
